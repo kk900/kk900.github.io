4116932514fc42c34365aa6072c6ed0e6a60abbd
kk900 <generikk@outlook.com>
2020-08-30 11:31:39 +0300

    update content and styles

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-self.addEventListener('install', event => event.waitUntil(caches.open('kk-pwa-cache').then(cache => cache.addAll(['/', '/index.html']))));
+self.addEventListener('install', event => event.waitUntil(caches.open('kk-pwa-cache')
+    .then(cache => cache.addAll([
+        '/',
+        '/index.html',
+        '/favicon.ico',
+        '/favicon-16.png',
+        '/favicon-32.png',
+        '/favicon-192.png',
+        '/favicon-384.png',
+    ]))));
